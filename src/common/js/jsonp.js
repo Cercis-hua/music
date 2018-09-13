c9d4ac580c5add2e7b88dcbe070a108b82a53d31
焦京花 <jiaojinghua@up366.com>
2018-09-13 11:21:44 +0800

    配置jsonp

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,11 +1,14 @@
+// 原始的jsonp
 import originJsonp from 'jsonp'
 
+// 对外暴露一个方法jsonp
 export default function jsonp(url, data, option) {
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
 
   return new Promise((resolve, reject) => {
     originJsonp(url, option, (err, data) => {
       if (!err) {
+        // 成功的 返回数据resolve出去
         resolve(data)
       } else {
         reject(err)
@@ -16,7 +19,7 @@ export default function jsonp(url, data, option) {
 
 export function param(data) {
   let url = ''
-  for (var k in data) {
+  for (let k in data) {
     let value = data[k] !== undefined ? data[k] : ''
     url += '&' + k + '=' + encodeURIComponent(value)
   }
