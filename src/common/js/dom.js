e1639de3889f22bf860efa0ba60a07febd0cd6f3
焦京花 <jiaojinghua@up366.com>
2018-09-13 17:25:38 +0800

    添加banner图

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,13 +1,15 @@
+// hasClass是否有class
 export function hasClass(el, className) {
   let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
   return reg.test(el.className)
 }
 
+// addClass添加class类名
 export function addClass(el, className) {
+  console.log('9090')
   if (hasClass(el, className)) {
     return
   }
-
   let newClass = el.className.split(' ')
   newClass.push(className)
   el.className = newClass.join(' ')
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="recommend">
     <div class="recommend-comtent">
-      <div class="slider-wrapper">
-
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
@@ -17,8 +23,14 @@
 <script type="text/ecmascript-6">
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
+  import Slider from 'base/slider/slider'
 
   export default {
+      data(){
+        return {
+          recommends: []
+        }
+      },
       created(){
         this._getRecommend()
       },
@@ -27,10 +39,14 @@
               // 请求数据
               getRecommend().then((res)=>{
                   if(res.code === ERR_OK){
-                    console.log(res.data)
+                    // console.log(res.data.slider)
+                    this.recommends = res.data.slider
                   }
               })
           }
+      },
+      components: {
+          Slider
       }
   }
 </script>
